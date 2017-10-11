92594d2ba3c354c287551cf5e6d5f1a992941e7c
Graham Hemsley <ghemsley@protonmail.ch>
2017-10-11 09:07:30 -0500

    Implemented --Universally_unique - users can now deduplicate all elements of the resulting cartesian product

--- cartesian-product.py
+++ cartesian-product.py
@@ -2,16 +2,18 @@
 
 from argparse import ArgumentParser
 from itertools import product
+from collections import Counter
 
 def createArgumentParser():
 
     parser = ArgumentParser()
     parser.add_argument("list", type=list, nargs="+", help="List(s) to compute the cartesian product of")
     parser.add_argument("-u", "--unique", action="store_true", help="Deduplicate lists so that they become sets of unique elements")
+    parser.add_argument("-U", "--Universally_unique", action="store_true", help="Deduplicate the resulting cartesian product so it becomes a set of unique elements")
     return parser.parse_args()
 
 
-def cartesianProduct(unique, arglist):
+def cartesianProduct(unique, Universally_unique, arglist):
 
     cartesianProduct = product([])
 
@@ -21,13 +23,22 @@ def cartesianProduct(unique, arglist):
         cartesianProduct = product(*arglist)
     else:
         cartesianProduct = product(*arglist)
+    if Universally_unique:
+        cartesianProduct = sorted(set(cartesianProduct))
+        for element in cartesianProduct:
+            for i in element:
+                if element.count(i) > 1:
+                    try:
+                        cartesianProduct.remove(element)
+                    except ValueError:
+                        pass
     return cartesianProduct
 
 
 def main():
 
     args = createArgumentParser()
-    for element in cartesianProduct(args.unique, args.list):
+    for element in cartesianProduct(args.unique, args.Universally_unique, args.list):
         print(element)
 
 
